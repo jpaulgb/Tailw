f4b6ddb03cee60b2dd87ae14065adde5176e3710
jpaulgb <jpaulgb@hotmail.com>
2025-07-08 19:47:32 -0500

    Beta with Grid Ok

--- Tailw/src/components/PokemonCard.jsx
+++ Tailw/src/components/PokemonCard.jsx
@@ -4,7 +4,8 @@ export default function PokemonCard({pokemon}){
     return(
         <div className="bg-white shadow-md rotate-lg overflow-hidden text-center p-4 transition hover:scale-105">
             <img src={pokemon.sprites.front_default} alt={pokemon.name} className="w-24 h-24 mx-auto"/>        
-           
+           <h1>{pokemon.name}</h1>
+           <h2>{pokemon.habilities}</h2>
         </div>
     )
 }
