a26c2c363be8d00e6d0b0a5a8adda67c637a5eff
jpaulgb <jpaulgb@hotmail.com>
2025-07-08 19:36:35 -0500

    Beta Ok

--- Tailw/src/components/PokemonCard.jsx
+++ Tailw/src/components/PokemonCard.jsx
@@ -1,9 +1,10 @@
 export default function PokemonCard({pokemon}){
     //<img src={pokemon.sprites.front_default} alt={pokemon.name} className="w-24 h-24 mx-auto" />
+    // <img className="card-img-top p-3" src={pokemon.sprites.front_default} alt={pokemon.name} />
     return(
         <div className="bg-white shadow-md rotate-lg overflow-hidden text-center p-4 transition hover:scale-105">
-            
-            <img className="card-img-top p-3" src={pokemon.sprites.front_default} alt={pokemon.name} />
+            <img src={pokemon.sprites.front_default} alt={pokemon.name} className="w-24 h-24 mx-auto"/>        
+           
         </div>
     )
 }
--- Tailw/src/main.jsx
+++ Tailw/src/main.jsx
@@ -12,7 +12,6 @@ import App from './App.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-    <PokemonCard />
     <PokemonList /> 
   </StrictMode>,
 )
